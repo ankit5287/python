015487872472fcf93894ca54faea8841de47f301
Ankit53 <formicro32@gmail.com>
2026-06-30 00:29:52 +0530

    Update closure file

--- closure.py
+++ closure.py
@@ -5,3 +5,16 @@ def outer(x):
 closure = outer(2)
 
 print(closure(9))
+
+def multipler(x):
+    def multiply_by(y):
+        return x*y
+    return multiply_by
+
+c1 = multipler(6)
+print(c1(7))
+
+double = multipler(2)
+print(c1(6))
+
+print(double.__closure__[0].cell_contents)
